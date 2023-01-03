18d3cc4778daba78e2a2053e73ea65d6b9210612
Jinho <thiszino@gmail.com>
2023-01-03 12:19:27 +0900

    network commit

---
+++ src/edu/kh/network/run/ServerRun.java
@@ -0,0 +1,11 @@
+package edu.kh.network.run;
+
+import edu.kh.network.model.service.TCPServer;
+
+public class ServerRun {
+
+	public static void main(String[] args) {
+		TCPServer server = new TCPServer();
+		server.severStart();
+	}
+}
